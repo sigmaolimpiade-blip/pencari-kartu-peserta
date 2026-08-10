d4feb6212580c668ad0c18f6a85c8f0a67617bef
sigmaolimpiade-blip <sigmaolimpiade@gmail.com>
2026-08-10 11:06:35 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbycf0du-iBsUPKMWSu7qat71e2HJfJIowhToTYKZIwQWjERWKoq_E_3P1F2J6cxyg2DJg/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby7Wmc99-B0w5p16EflJPhLP7auk5VNwrvR85z4bxgNxDvoPkAyeJKjhC6ZyJgwRX1QHw/exec";
 
 document.getElementById('searchForm').addEventListener('submit', async function(e) {
   e.preventDefault();
